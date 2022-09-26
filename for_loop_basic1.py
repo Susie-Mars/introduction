d48d975881cec3e1aab7313abc50410e4420e1fa
Susie-Mars <marsnarsbars@gmail.com>
2022-09-26 13:39:38 -0600

    for loop basics

---
+++ for_loop_basic1.py
@@ -0,0 +1,37 @@
+# BASIC
+for i in range(151):
+    print(i)
+
+# MULTIPLES OF FIVE
+for i in range(5,1001,5):
+    print(i)
+
+# COUNTING, THE DOJO WAY
+for i in range(1,100+1):
+    if (i%10==0):
+        print("Coding Dojo")
+    elif (i%5==0):
+        print("Coding")
+    else:
+        print(i)
+
+
+# WOAH, THAT SUCKERS HUGE
+sum = 0
+for i in range(0,50000+1):
+    if (i%2!=0):
+        sum+=i
+print(sum)
+
+# COUNTDOWN BY FOURS
+for i in range(2018,0,-4):
+    print(i)
+
+# FLEXIBLE COUNTER
+lowNum = 2
+highNum = 9
+mult = 3
+
+for i in range(lowNum, highNum+1):
+    if (i%2!=0):
+        print(i)
